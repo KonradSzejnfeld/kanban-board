0f954ca380a785033aad6e652edfd95bc7e7a5ba
KonradSzejnfeld <konrad.szejnfeld@gmail.com>
2017-12-15 03:35:22 +0100

    Adding kanban board files

---
+++ App.js
@@ -0,0 +1,32 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': "697",
+  'X-Auth-Token': "02805b6b670057e40ecd453780d777fa",
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	   cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	});
+}
